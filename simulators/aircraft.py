0996ffb8d069e3f0d6da5d8a13b5a9c5c63087de
AlviPat9 <79159554+AlviPat9@users.noreply.github.com>
2023-05-12 11:44:28 +0200

    Update aircraft.py
    Added method to calculate latitude and longitude

--- simulators/aircraft.py
+++ simulators/aircraft.py
@@ -11,6 +11,10 @@ Author: Alvaro Marcos Canedo
 
 from abc import ABC, abstractmethod
 
+from Ares.aircraft_simulators.atmospheric_model import AtmosphericModel
+
+from numpy import sin, cos, sqrt
+
 
 class Aircraft(ABC):
     """
@@ -30,6 +34,43 @@ class Aircraft(ABC):
 
         # Definition of constants
         self.g = 9.81
+        self.R = 6378137.0
+        self.f = 1 / 298.257222101
+        self._b = self.R * (1 - self.f)
+        self.e2 = 1 - (1 - self.f ** 2)
+
+        # Instantiate atmospheric model
+        self.atmosphere = AtmosphericModel()
+
+    def earth_position(self, lat: float, lon: float, x: float, y: float, z: float) -> tuple:
+        """
+
+        Method to calculate the earth position of the aircraft.
+
+        @param lat: Current latitude of the aircraft.
+        @type lat: float
+        @param lon: Current longitude of the aircraft.
+        @type lon: float
+        @param x: x Position of the aircraft.
+        @type x: float
+        @param y: y Position of the aircraft.
+        @type y: float
+        @param z: z Position of the aircraft.
+        @type z: float
+
+        @return: Latitude and longitude rates.
+        @rtype: tuple
+        """
+
+        # Intermediate calculus
+        v = self.R / sqrt((1 - self.e2 * sin(lat) ** 2))
+        r = v * (1.0 - self.e2) / (1.0 - self.e2 * sin(lat) ** 2)
+
+        # Calculation of latitude and longitude rates
+        latp = x / (r + z)
+        lonp = y / (cos(lat) * (self.R + z))
+
+        return latp, lonp
 
     @abstractmethod
     def _sensors(self, *args):
@@ -193,7 +234,7 @@ class Aircraft(ABC):
         @param args: Additional arguments of the function.
 
         """
-        
+
         raise NotImplementedError('As it is an abstract method, it should be overwritten in the appropriate subclass.')
 
 
