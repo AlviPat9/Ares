e8d01bf84ae90e5c3cf2b91993b58d920b22e8f5
Álvaro Marcos Canedo <79159554+AlviPat9@users.noreply.github.com>
2025-09-18 12:54:41 +0200

    Update rk45.py
    Corrected Adaptative time step control

--- integrators/rk45.py
+++ integrators/rk45.py
@@ -26,16 +26,16 @@ class RK45(BaseIntegrator):
 
         Constructor method
 
-        @param system: System to integrate over time.
-        @type system: object
-        @param step_size: Step size for the integration.
-        @type step_size: float
-        @param final_time: Final time for the integration.
-        @type final_time: float
-        @param initial_state: Initial state for the system.
-        @type initial_state: numpy.ndarray
-		@param tol: Tolerance value for the model.
-		@type tol: float
+        :param system: System to integrate over time.
+        :type system: object
+        :param step_size: Step size for the integration.
+        :type step_size: float
+        :param final_time: Final time for the integration.
+        :type final_time: float
+        :param initial_state: Initial state for the system.
+        :type initial_state: numpy.ndarray
+        :param tol: Tolerance value for the model.
+        :type tol: float
 
         """
 
@@ -45,16 +45,22 @@ class RK45(BaseIntegrator):
         self.time = 0.0
 		self.tol = tol
 
+        # Safety factor and limits for a robus step size control
+        self.safety_factor = 0.9
+        self.max_step_increase = 5.0
+        self.min_step_decrease = 0.2
+        self.max_iteration = 20
+
     def integrate(self, *args: object):
         """
 
         Method to integrate for the entire time defined the model.
 
-        @param args: additional arguments of the function.
-        @type args: object
+        :param args: additional arguments of the function.
+        :type args: object
 
-        @return Complete state of the model during the simulation.
-        @rtype np.ndarray
+        :return Complete state of the model during the simulation.
+        :rtype np.ndarray
         """
         complete_state = self._state
         n = int((self._final_time - self.time) / self._step_size) + 1
@@ -70,39 +76,77 @@ class RK45(BaseIntegrator):
 
         Method to integrate one step of the model.
 
-        @param args: additional arguments of the function.
-        @type args: object
+        :param args: additional arguments of the function.
+        :type args: object
 
         """
-        k1 = self._step_size * self._system(self.time, self._state, *args)
-        k2 = self._step_size * self._system(self.time + self._step_size * 0.25, self._state + k1 * 0.25, *args)
-        k3 = self._step_size * self._system(self.time + self._step_size * 0.375, self._state + k1 * 3/32 + k2 * 9/32, *args)
-        k4 = self._step_size * self._system(self.time + self._step_size * 12/13, self._state + k1 * 1932/2197 - k2 * 7200/2197 + k3 * 7296/2197, *args)
-		k5 = self._step_size * self._system(self.time + self._step_size, self._state + k1 * 439/216 - 8*k2 + k3 * 3680/513 - k4 * 845/4104, *args)
-		k6 = self._step_size * self._system(self.time + self._step_size * 0.5, self._state - k1 * 8/27 + k2 * 2 - k3 * 3544/2565 + k4 * 1859/4104 - k5 * 11/40, *args)
+        # Initialize the number of iterations
+        iteration = 0
 
-		# Compute new state based on Runge Kutta 4 and 5 methods
-        y_new = self._state + k1 * 25/216 + k3 * 1408/2565 + k4 * 2197/4104 - k5 * 1/5
-		y_hat = self._state + k1 * 16/135 + k3 * 6656/12825 + k4 * 28561/56430 - k5 * 9/50 + k6 * 2/55
-		
-		# Update current state
-		self._state = y_new
-		
-		# Update current time
-		self.time += self._step_size
+        while True:
+            # Calculate the characteristic values of the runge kutta
+            k1 = self._step_size * self._system(self.time, self._state, *args)
+            k2 = self._step_size * self._system(self.time + self._step_size * 0.25, self._state + k1 * 0.25, *args)
+            k3 = self._step_size * self._system(self.time + self._step_size * 0.375, self._state + k1 * 3/32 + k2 * 9/32, *args)
+            k4 = self._step_size * self._system(self.time + self._step_size * 12/13, self._state + k1 * 1932/2197 - k2 * 7200/2197 + k3 * 7296/2197, *args)
+            k5 = self._step_size * self._system(self.time + self._step_size, self._state + k1 * 439/216 - 8*k2 + k3 * 3680/513 - k4 * 845/4104, *args)
+            k6 = self._step_size * self._system(self.time + self._step_size * 0.5, self._state - k1 * 8/27 + k2 * 2 - k3 * 3544/2565 + k4 * 1859/4104 - k5 * 11/40, *args)
 
-		# Check error between both states to update time step size
-		self.__update_time_step(y_new, y_hat)
+            # Compute new state based on Runge Kutta 4 and 5 methods
+            y_4th = self._state + k1 * 25/216 + k3 * 1408/2565 + k4 * 2197/4104 - k5 * 1/5
+            y_5th = self._state + k1 * 16/135 + k3 * 6656/12825 + k4 * 28561/56430 - k5 * 9/50 + k6 * 2/55
+            
+            # Compute error
+            error = np.linalg.norm(y_5th - y_4th)
 
-        return self._state
+            # Avoid division by 0
+            if error < 1e-15:
+                error = 1e-15
+            
+            # Adaptative Step control
+            # Calculate optimal time step
+            h_opt = self.safety_factor * self._step_size * (self.tol / error) ** 0.2
+
+            # Set limitation for max increase and min decrease
+            if h_opt > self._step_size * self.max_step_increase:
+                h_opt = self._step_size * self.max_step_increase:
+            elif h_opt < self._step_size * self.min_step_decrease:
+                h_opt = self._step_size * self.min_step_decrease
+
+            # Accept or reject the new step
+            if error <= self.tol:
+                # Time step has been accepted
+                # Update time
+                self.time += self._step_size
+                
+                # Update new time step
+                self._state = y_5th
+
+                # Update new Time step
+                self._step_size = h_opt
+
+                # Return the state
+                return self._state
+            else:
+                # Time step has not been accepted
+                # Re-do de simulation with the optimal time step calculated (without updating the state)
+                self._step_size = h_opt
+                iteration +=1
+
+                 if iteration > self.max_iteration:
+                    raise RuntimeError(
+                        f"RK45 integrator failed. Step size has been reduced "
+                        f"{self.max_iteration} times without meeting the error tolerance "
+                        f"at t={self.time}. The system may be stiff or a singularity may be present."
+                    )
 
     def get_step_size(self):
         """
 
         Method to get the step size of the model
 
-        @return: Step size defined for the model
-        @rtype: float
+        :return: Step size defined for the model
+        :rtype: float
 
         """
 
@@ -113,8 +157,8 @@ class RK45(BaseIntegrator):
 
         Method to set the value for the step size of the model.
 
-        @param step_size: Step size for the model.
-        @type step_size: float
+        :param step_size: Step size for the model.
+        :type step_size: float
 
         """
 
@@ -129,7 +173,7 @@ class RK45(BaseIntegrator):
 
         Method to get the current state of the model.
 
-        @return Current state of the system.
+        :return Current state of the system.
 
         """
 
@@ -140,34 +184,13 @@ class RK45(BaseIntegrator):
 
         Method to set the state of the model.
 
-        @param state: state of the model.
-        @type state: numpy.ndarray
+        :param state: state of the model.
+        :type state: numpy.ndarray
 
         """
         # TODO -> Implement a method to check if the new state has same number as the functions passed as arguments.
 
         self._state = state
-	
-	def __update_time_step(self, y_new: np.ndarray, y_hat: np.ndarray):
-		"""
-		
-		Method to compute the erro estimate and adjust the step size using the error estimate and the tolerance.
-		
-		@param y_new: Runge-kutta fourth order method.
-		@type y_new: np.ndarray
-		@param y_hat: Runge-kutta approximation.
-		@type y_hat: np.ndarray
-		
-		"""
-		
-		# Calculate maximum error for the current state -> based on the Runge Kutta 4 and other approximation method.
-		error = np.max(np.abs(y_hat - y_new))
-		
-		# Compute new time_step
-		new_step = self._step_size * (self.tol / error) ** 0.2
-		
-		# Update current step size
-		self._step_size = min(new_step, self.final_time - self.time)	
 		
 
 __all__ = ["RK45"]
